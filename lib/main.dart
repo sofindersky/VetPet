f6f162c1334a42f43ed36b21e268eff2d66fc1db
Sofiia Servetnyk <sophie.villansky@gmail.com>
2024-08-20 13:28:24 +0300

    small fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pet_vet_project/src/app.dart';
+import 'package:pet_vet_project/src/core/helper/supported_locales.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +10,7 @@ void main() async {
   runApp(
     ProviderScope(
       child: EasyLocalization(
-        supportedLocales: const [Locale('en'), Locale('uk')],
+        supportedLocales: SupportedLocales.supportedLocales,
         fallbackLocale: const Locale('uk'),
         path: 'assets/translations',
         child: const MyApp(),
